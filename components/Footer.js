87a9ce3c081d37c0a261b772bae99e377ad3689a
nunonam <nunonam@gmail.com>
2022-10-10 11:49:08 +0900

    update

--- components/Footer.js
+++ components/Footer.js
@@ -1,19 +1,18 @@
 import React from 'react'
 
-import { Box, HStack, Link, LinkOverlay, Text, useColorModeValue } from '@chakra-ui/react'
+import { Box, Button, HStack, Link, LinkOverlay, Text, useColorModeValue } from '@chakra-ui/react'
+import { useRouter } from 'next/router';
 
 export default function Footer() {
+  const router = useRouter();
   const borderColor = useColorModeValue('gray.200', 'gray.700');
   return (
     <HStack justifyContent={'space-between'} borderTopWidth={1} borderTopColor={borderColor} mt={6} pt={6}>
 
       <HStack>
-        <Link href='https://www.chatty-cloud.com/legal/privacy'>
-          <Text fontSize={'sm'}>Privacy</Text>
-        </Link>
-        <Link href='https://www.chatty-cloud.com/legal/siteterms'>
-          <Text fontSize={'sm'}>Terms</Text>
-        </Link>
+        <Button variant={'link'} size='xs' onClick={() => window.open('https://app.termly.io/document/privacy-policy/d94c196e-2f71-4137-a3ea-e0f458064757', '_ blank')}>Privacy</Button>
+        <Button variant={'link'} size='xs' onClick={() => window.open('https://app.termly.io/document/terms-of-use-for-saas/fc9e8007-f21e-4d94-ac2e-01848b5ec7fb#agreement', '_ blank')}>Terms</Button>
+        <Button variant={'link'} size='xs' onClick={() => window.open('https://app.termly.io/document/cookie-policy/578b6f0b-18f3-4690-800d-6126e0ff898d', '_ blank')}>Cookie</Button>
       </HStack>
       <Text fontSize={'sm'} fontWeight={'extrabold'}>&copy; 2021 ChattyCloud Inc.</Text>
     </HStack>
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -34,6 +34,13 @@ export default class Document extends NextDocument {
           <meta content='IE=edge' httpEquiv='X-UA-Compatible' />
           <meta content='#262626' name='theme-color' />
           <link rel='shortcut icon' href='/favicon.svg' />
+          <script
+            type="text/javascript"
+            src="https://app.termly.io/embed.min.js"
+            data-auto-block="on"
+            data-website-uuid="3eb2e29f-9e88-4035-a719-f09b516d6191"
+            async
+          />
           <script dangerouslySetInnerHTML={{ __html: this.renderSnippet() }} />
         </Head>
         <body>
