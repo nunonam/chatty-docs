b2a2cfd4f82c068ff5bcb8fb1b8a4c3221a521d0
nunonam <nunonam@gmail.com>
2022-10-23 13:29:13 +0900

    update

--- components/MDX.AnchorLink.js
+++ components/MDX.AnchorLink.js
@@ -1,74 +1,14 @@
 import * as React from 'react'
-import { Box, Heading, LinkBox, LinkOverlay, Text, useColorModeValue } from '@chakra-ui/react'
-import Link from 'next/link'
+import { Box, useColorModeValue } from '@chakra-ui/react'
+import theme from './Theme';
 
 
-function AnchorLink(props) {
-  const textColor = useColorModeValue('blue.600', 'blue.400');
-
-  const { children, heading, category } = props
-
-  console.log('link nuno', children)
+function AnchorLink({ children }) {
+  const color = useColorModeValue(theme.colors.blue[600], theme.colors.blue[500]);
   return (
-    <Box as={'span'} fontSize={'sm'} color={'blue.300'}>
-      <a>{props.children}</a>
-      {/* <Link href={'/'}>{props.children}</Link> */}
+    <Box as={'span'} fontSize={'sm'} color={color}>
+      <a href={children} target='_blank'><b>{children}</b></a>
     </Box>
-    // <LinkBox
-    //   // as={'span'}
-    //   fontSize='sm'
-    //   fontWeight={'bold'}
-    //   bgColor={'blue.100'}
-    // >
-    //   {Object.prototype.toString.call(children) === '[object Array]' && children.map((item, index) => {
-    //     console.log('nuno', item);
-    //     return (
-    //       <Link href={'/'} passHref key={index}>
-    //         <LinkOverlay>
-    //           <Box borderRadius={'md'} p={2} bgColor={'blue.100'}>
-    //             <a>dfsdf</a>
-    //             <Text
-    //               fontSize={'sm'}
-    //               color={textColor}
-    //             >
-    //               {item}
-    //             </Text>
-    //           </Box>
-    //         </LinkOverlay>
-    //       </Link>
-    //     )
-    //   })}
-
-
-    //   {Object.prototype.toString.call(children) === 'object' && (
-    //     <Link href={'/'} passHref>
-    //       <LinkOverlay>
-    //         <Box borderRadius={'md'} p={2} bgColor={'blue.100'}>
-    //           <Text
-    //             fontSize={'sm'}
-    //             color={textColor}
-    //           >
-    //             {children.props.children}
-    //           </Text>
-    //         </Box>
-    //       </LinkOverlay>
-    //     </Link>
-    //   )}
-    //   {children === 'string' && (
-    //     <Link href={'/'} passHref>
-    //       <LinkOverlay>
-    //         <Box borderRadius={'md'} p={2} bgColor={'blue.100'}>
-    //           <Text
-    //             fontSize={'sm'}
-    //             color={textColor}
-    //           >
-    //             {children}
-    //           </Text>
-    //         </Box>
-    //       </LinkOverlay>
-    //     </Link>
-    //   )}
-    // </LinkBox >
   );
 }
 
